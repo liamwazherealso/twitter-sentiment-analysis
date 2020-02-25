da767d653a6120c9472b6c71626c517c50020cfd
Liam Pieri <liamwazherealso@gmail.com>
2020-02-25 18:07:44 -0500

    Add pre-trained notebook

--- src/data/download_dataset.sh
+++ src/data/download_dataset.sh
@@ -1,1 +1,6 @@
-kaggle kernels output paoloripamonti/twitter-sentiment-analysis -p models
+export KAGGLE_CONFIG_DIR="/tf/twitter-sentiment-analysis/kaggle"
+if [ ! -f ../data/raw/training.1600000.processed.noemoticon.csv ];then	
+	kaggle datasets download -d kazanova/sentiment140  -p ../data/raw
+	unzip ../data/raw/*.zip -d ../data/raw
+	rm ../data/raw/*.zip
+fi
--- src/models/download_models.sh
+++ src/models/download_models.sh
@@ -1,1 +1,3 @@
+export KAGGLE_CONFIG_DIR="/tf/twitter-sentiment-analysis/kaggle"
 kaggle kernels output paoloripamonti/twitter-sentiment-analysis/output -p ../../models
+
