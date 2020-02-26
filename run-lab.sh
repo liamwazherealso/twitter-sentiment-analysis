1f77b8549b7eeb8355ba086523288734e3b2a9ae
Liam Pieri <liamwazherealso@gmail.com>
2020-02-26 14:10:55 -0500

    Add gpu usage line to run-lab

--- run-lab.sh
+++ run-lab.sh
@@ -1,1 +1,1 @@
-docker run -it --volume $PWD:/tf/twitter-sentiment-analysis -p 8888:8888 tensorflow/tensorflow:nightly-gpu-py3-jupyter
+docker run --gpus all -it --volume $PWD:/tf/twitter-sentiment-analysis -p 8888:8888 tensorflow/tensorflow:nightly-gpu-py3-jupyter
